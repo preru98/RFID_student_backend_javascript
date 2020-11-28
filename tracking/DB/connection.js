a3f6ab324e3d5aa6fdc6baa73dcd4d9b1408076a
Prerna <prerna1998.ps@gmail.com>
2020-11-28 23:56:58 +0530

    Student Schema Demo

--- tracking/DB/connection.js
+++ tracking/DB/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const URI = 'mongodb+srv://*****:*****@*****.gyh5q.mongodb.net/*****?retryWrites=true&w=majority';
+const URI = 'mongodb+srv://Prerna15:*****@**.gyh5q.mongodb.net/test?retryWrites=true&w=majority';
 
 module.exports = connectDB = () => {
     mongoose.connect(URI, {
---
+++ tracking/models/student.js
@@ -0,0 +1,17 @@
+const mongoose = require ('mongoose');
+const Schema = mongoose.Schema
+
+const studentSchema = Schema(
+    {
+        name : {
+            type : String, 
+            required : true,
+        },
+        rollnumber : {
+            type : String, 
+            required : true,
+        },
+    }
+);
+
+module.exports=mongoose.model("Student", studentSchema);
